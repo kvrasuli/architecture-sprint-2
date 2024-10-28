e3d9abc722274c1fc9a38ff547232e57d060cba2
Kirill Rasuli <kirill.rasuli@south.rt.ru>
2024-10-28 18:37:45 +0300

    Fix script

--- mongo-sharding/mongo-init.sh
+++ mongo-sharding/mongo-init.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-docker exec configSrv mongosh --port 27020 <<EOF
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
 rs.initiate(
   {
     _id : "config_server",
     configsvr: true,
     members: [
-      { _id : 0, host : "configSrv:27020" }
+      { _id : 0, host : "configSrv:27017" }
     ]
   }
 );
 exit();
 EOF
 
-docker exec shard1 mongosh --port 27018 <<EOF
+docker compose exec -T shard1 mongosh --port 27018 <<EOF
 rs.initiate(
     {
       _id : "shard1",
@@ -26,7 +26,7 @@ rs.initiate(
 exit();
 EOF
 
-docker exec shard2 mongosh --port 27019 <<EOF
+docker compose exec -T shard2 mongosh --port 27019 <<EOF
 rs.initiate(
     {
       _id : "shard2",
@@ -39,7 +39,7 @@ rs.initiate(
 exit();
 EOF
 
-docker exec mongos_router mongosh --port 27017 <<EOF
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
 sh.addShard( "shard1/shard1:27018");
 sh.addShard( "shard2/shard2:27019");
 sh.enableSharding("somedb");
