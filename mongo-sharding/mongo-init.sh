4104da33296f67a07ba4f2039ff1cef3479615f7
Kirill Rasuli <kirill.rasuli@south.rt.ru>
2024-10-28 17:25:21 +0300

    Add sh script to set up sharding

---
+++ mongo-sharding/mongo-init.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+docker exec configSrv mongosh --port 27020 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+    configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27020" }
+    ]
+  }
+);
+exit();
+EOF
+
+docker exec shard1 mongosh --port 27018 <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+       // { _id : 1, host : "shard2:27019" }
+      ]
+    }
+);
+exit();
+EOF
+
+docker exec shard2 mongosh --port 27019 <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+       // { _id : 0, host : "shard1:27018" },
+        { _id : 1, host : "shard2:27019" }
+      ]
+    }
+  );
+exit();
+EOF
+
+docker exec mongos_router mongosh --port 27017 <<EOF
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+db.helloDoc.countDocuments()
+exit();
+EOF
